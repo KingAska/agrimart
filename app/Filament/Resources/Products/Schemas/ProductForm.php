e03e4650255644b6e31acd95c6ba74705c25de05
KingAska <azz141095@gmail.com>
2026-05-21 13:12:48 +0700

    update automatic slug

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -26,8 +26,7 @@ class ProductForm
                     TextInput::make('name')
                         ->required()
                         ->live(onBlur: true)
-                        ->afterStateUpdated(fn (string $operation, $state, Set $set) => $operation === 'create' ? $set('slug', Str::slug($state)) : null),
-
+                        ->afterStateUpdated(fn ($state, Set $set) => $set('slug', Str::slug($state))),
                     TextInput::make('slug')
                         ->disabled()
                         ->dehydrated()
